37b4a48b2efc4528174d8f02bd39ac51b94da8ab
uvardic <uros.vardiic@gmail.com>
2019-12-23 18:43:56 +0100

    components

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,8 +1,27 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {MachinesComponent} from './machines/machines.component';
 
 const routes: Routes = [
-    {}
+    {
+        path: '',
+        redirectTo: '/machines',
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'machines',
+        component: MachinesComponent
+    }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { AlertModule } from 'ngx-bootstrap';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AlertModule} from 'ngx-bootstrap';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.components';
-import { AppRouterModule } from './app-router.module';
+import {AppComponent} from './app.components';
+import {AppRouterModule} from './app-router.module';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {MachinesComponent} from './machines/machines.component';
+import {MenuComponent} from './menu/menu.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        RegisterComponent,
+        LoginComponent,
+        MachinesComponent,
+        MenuComponent
     ],
     imports: [
         AlertModule.forRoot(),
         BrowserModule,
-        AppRouterModule
+        AppRouterModule,
+        FormsModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
